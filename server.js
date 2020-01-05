f2b2e370a10690e0cd27a423871dfa2b15de42fc
jstone18 <jason.stone0808@gmail.com>
2020-01-05 15:42:11 -0500

    Update server.js with mock db, root, signin, register  and profile routes

--- server.js
+++ server.js
@@ -2,8 +2,74 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const db = {
+	users: [
+		{
+			id: "1",
+			name: "John",
+			email: "john@gmail.com",
+			password: "foobar123",
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: "2",
+			name: "Jane",
+			email: "jane@gmail.com",
+			password: "foobaz123",
+			entries: 0,
+			joined: new Date()
+		}
+	]
+};
+
+// Root Route
 app.get("/", (req, res) => {
-	res.send("this is working...");
+	res.send(db.users);
+});
+
+// Signin Route
+app.post("/signin", (req, res) => {
+	if (
+		req.body.email === db.users[0].email &&
+		req.body.password === db.users[0].password
+	) {
+		res.json("success");
+	} else {
+		res.status(400).json("error logging in");
+	}
+	res.json("signin");
+});
+
+// Register Route
+app.post("/register", (req, res) => {
+	const { name, email, password } = req.body;
+	db.users.push({
+		id: "3",
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	});
+	res.json(db.users[db.users.length - 1]);
+});
+
+// Profile
+app.get("/profile/:id", (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	db.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		}
+	});
+	if (!found) {
+		res.status(400).json("not found");
+	}
 });
 
 app.listen(3000, () => {
